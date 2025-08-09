d7e328b86d3343d746e2189fea66ea5440134358
brianlien95 <ppap14577@gmail.com>
2025-08-09 10:54:48 +0800

    Site updated: 2025-08-09 10:54:47

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1754503647042";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1754708085249";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
