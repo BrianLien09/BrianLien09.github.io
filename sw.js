43a28984cece9a8da52be3a853d112ae579a7a72
Brianlien09 <brianlien09@gmail.com>
2026-02-12 14:36:34 +0800

    Site updated: 2026-02-12 14:36:32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1768287334797";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1770878187979";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
